592fb66c4d169530e97e00df5dbe9b45178972ee
replikation <christian.jena@gmail.com>
2018-10-10 15:57:35 +0200

    changed step 4, now moves everything related to KPC_fusion_plasmids

--- kpc_4_removal_of_fusionplasmids.sh
+++ kpc_4_removal_of_fusionplasmids.sh
@@ -13,10 +13,16 @@ for x in Annotation_blastn/*.gff; do
 	if (($amount_of_KPCs > 1)); then
 		echo "found $amount_of_KPCs KPC genes in ${y%.gff}"
 		mv plasmid_files_fasta/${y%_resistance_genes.gff}.fasta KPC_fusion_plasmids/ 2>/dev/null
+		mv plasmid_files_gb/${y%_resistance_genes.gff}.gb KPC_fusion_plasmids/ 2>/dev/null
+		mv Annotation_blastn/${y%_resistance_genes.gff}* KPC_fusion_plasmids/ 2>/dev/null
+		mv Annotation_Prokka/${y%_resistance_genes.gff}* KPC_fusion_plasmids/ 2>/dev/null
 	else
 		if (($amount_of_Incs > 1)); then
 		echo "found $amount_of_Incs Inc groups in ${y%.gff}"
-                mv plasmid_files_fasta/${y%_resistance_genes.gff}.fasta KPC_fusion_plasmids/ 2>/dev/null
+                mv plasmid_files_fasta/${y%_resistance_genes.gff}* KPC_fusion_plasmids/ 2>/dev/null
+		mv plasmid_files_gb/${y%_resistance_genes.gff}* KPC_fusion_plasmids/ 2>/dev/null
+                mv Annotation_blastn/${y%_resistance_genes.gff}* KPC_fusion_plasmids/ 2>/dev/null
+                mv Annotation_Prokka/${y%_resistance_genes.gff}* KPC_fusion_plasmids/ 2>/dev/null
 		fi
 	fi
 done
